788fe563c69a0a94bdc6e589f5cbeeafe02304fc
Joshua Levy <joshua@cal.berkeley.edu>
2015-08-23 15:34:06 -0700

    Fix functools dep.

--- instaclone/main.py
+++ instaclone/main.py
@@ -10,7 +10,7 @@ import argparse
 import sys
 
 NAME = "instaclone"
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 DESCRIPTION = "instaclone: Fast, cached installations of versioned files"
 LONG_DESCRIPTION = __doc__
 
