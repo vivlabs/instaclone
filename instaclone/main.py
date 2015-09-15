650cb78be70683d8b664ba72625c19deadc8c089
Joshua Levy <joshua@cal.berkeley.edu>
2015-09-15 10:28:46 -0700

    Adjust install_requires for better compatibility.

--- instaclone/main.py
+++ instaclone/main.py
@@ -10,7 +10,7 @@ import argparse
 import sys
 
 NAME = "instaclone"
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 DESCRIPTION = "instaclone: Fast, cached installations of versioned files"
 LONG_DESCRIPTION = __doc__
 
